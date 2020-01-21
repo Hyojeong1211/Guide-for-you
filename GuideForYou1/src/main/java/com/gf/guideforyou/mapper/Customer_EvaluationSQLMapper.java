3fff2a109c8bca700f7547e3144d4c93905c4964
Hyojeong Kim <53035646+Hyojeong1211@users.noreply.github.com>
2020-01-21 17:44:28 +0100

    Update Customer_EvaluationSQLMapper.java

--- GuideForYou1/src/main/java/com/gf/guideforyou/mapper/Customer_EvaluationSQLMapper.java
+++ GuideForYou1/src/main/java/com/gf/guideforyou/mapper/Customer_EvaluationSQLMapper.java
@@ -17,11 +17,11 @@ public interface Customer_EvaluationSQLMapper {
 	@Insert("INSERT INTO GF_CUSTOMER_EVALUATION VALUES(GF_CUSTOMER_EVALUATION_SEQ.NEXTVAL,#{tour_idx},#{member_idx},#{customer_score},#{customer_review})")
 	public void insertCustomerEvaluation(Customer_EvaluationBasicVo vo);
 
-	// 고객의 총 평점을 구하는 쿼리문
+	
 	@Select("SELECT ROUND(AVG(CUSTOMER_SCORE)) FROM GF_CUSTOMER_EVALUATION WHERE MEMBER_IDX=#{member_idx}")
 	public int selectAvg_Score(int member_idx); 
 	
-	//투어 idx로 
+	
 	@Select("SELECT * FROM GF_CUSTOMER_EVALUATION WHERE TOUR_IDX=#{tour_idx}")
 	public ArrayList<Customer_EvaluationBasicVo> selectCustomerByTourIdx(Customer_EvaluationBasicVo vo);
 	
@@ -29,16 +29,15 @@ public interface Customer_EvaluationSQLMapper {
 	public void deleteReviewByCustomer_Idx(int customer_idx);
 	
 	
-	//추가된  sql문 입니다   유기조!!!
-	//member_idx로  유기조 변경
+	//added sql quary 
+	//changed to member idx
 		@Select("SELECT * FROM GF_CUSTOMER_EVALUATION WHERE MEMBER_IDX=#{member_idx}")
 		public ArrayList<Customer_EvaluationBasicVo> kijoSelectCustomerByMemberIdx(MemberBasicVo vo);
 		
-		// 고객의 총 평점을 구하는 쿼리문
 		@Select("SELECT ROUND(AVG(CUSTOMER_SCORE)) FROM GF_CUSTOMER_EVALUATION WHERE MEMBER_IDX=#{member_idx}")
 		public String kijoselectAvg_Score(int member_idx); 
 		
-		//유기조  2차로 추가된 것입니다
+		
 		@Select("SELECT * FROM GF_RESERVATION_INFO WHERE TOUR_IDX=#{tour_idx} AND RESERVATION_CONFIRM ='y' AND MEMBER_IDX NOT IN(SELECT MEMBER_IDX FROM GF_CUSTOMER_EVALUATION WHERE TOUR_IDX=#{tour_idx})")
 		public ArrayList<Customer_EvaluationBasicVo>  kijoSelectCustomerByMemberTour(ReservationBasicVo vo);
 }
